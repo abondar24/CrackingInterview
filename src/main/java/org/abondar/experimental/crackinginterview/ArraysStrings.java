34f82be779f782e9218139d3c96979fa0cde01e6
abondar24 <abondar1992@gmail.com>
2017-08-30 17:32:23 +0200

    isUnique task

--- src/main/java/org/abondar/experimental/crackinginterview/ArraysStrings.java
+++ src/main/java/org/abondar/experimental/crackinginterview/ArraysStrings.java
@@ -1,4 +1,32 @@
 package org.abondar.experimental.crackinginterview;
 
+import java.util.Arrays;
+
+
 public class ArraysStrings {
+
+    /**
+     * Determines if all the characters in string are unique
+     */
+    public boolean isUniqque(String str) {
+        boolean isUnique = false;
+
+        char [] strc = str.toCharArray();
+        Arrays.sort(strc);
+
+        for (int i = 0; i < strc.length-1; i++) {
+            if (strc[i] != strc[i + 1]) {
+                isUnique = true;
+            } else {
+                isUnique = false;
+                break;
+            }
+        }
+
+        System.out.println(isUnique);
+
+        return isUnique;
+    }
+
+    ;
 }
--- src/test/java/org/abondar/experimental/crackinginterview/tests/ArraysStringsTest.java
+++ src/test/java/org/abondar/experimental/crackinginterview/tests/ArraysStringsTest.java
@@ -1,6 +1,21 @@
 package org.abondar.experimental.crackinginterview.tests;
 
+import org.abondar.experimental.crackinginterview.ArraysStrings;
+import org.junit.Assert;
+import org.junit.Test;
+
 public class ArraysStringsTest {
 
-    
+    private ArraysStrings as = new ArraysStrings();
+
+    @Test
+    public void inUniqueTest(){
+
+        boolean res1 = as.isUniqque("hello world");
+        Assert.assertFalse(res1);
+
+        boolean res2 = as.isUniqque("formula");
+        Assert.assertTrue(res2);
+
+    }
 }
