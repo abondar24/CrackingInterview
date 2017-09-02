866872e826c794b5b2f47ffc825a5bacd5f62b0d
abondar24 <abondar1992@gmail.com>
2017-09-02 19:10:55 +0200

    K to Last

--- src/main/java/org/abondar/experimental/crackinginterview/Lists.java
+++ src/main/java/org/abondar/experimental/crackinginterview/Lists.java
@@ -4,14 +4,14 @@ public class Lists {
 
     /**
      * Remove duplicates from list without using any tmp buffer
-     * */
-    public Node removeDups(Node head){
+     */
+    public Node removeDups(Node head) {
 
         Node cur = head;
-        while (cur!=null){
+        while (cur != null) {
             Node runner = cur;
-            while (runner.getNext()!=null){
-                if (runner.getNext().getData()==cur.getData()){
+            while (runner.getNext() != null) {
+                if (runner.getNext().getData() == cur.getData()) {
                     runner.setNext(runner.getNext().getNext());
                 } else {
                     runner = runner.getNext();
@@ -21,7 +21,39 @@ public class Lists {
             cur = cur.getNext();
         }
 
-
         return head;
+    }
+
+    /**
+     * Return  k elements from last element in list
+     * I count positions from 0
+     * if list is 1,2,3 and k=1 or 0 -> return 3; k=2 -> return 2,3
+     */
+    public Node kToLast(Node head, int k) {
+        Node kToLast = null;
+
+        Node cur = head;
+        int size = 0;
+
+        while (cur != null) {
+            cur = cur.getNext();
+            size++;
+        }
+
+        // if we count from 1 posToRet = size -k +1; curPos =1
+        int posToRet =  size-k;
+        int curPos = 0;
+
+        cur = head;
+        while (cur != null) {
+
+            if (curPos==posToRet){
+                kToLast = cur;
+            }
+            cur = cur.getNext();
+            curPos++;
+        }
+
+        return kToLast;
     }
 }
--- src/test/java/org/abondar/experimental/crackinginterview/tests/ListsTest.java
+++ src/test/java/org/abondar/experimental/crackinginterview/tests/ListsTest.java
@@ -5,6 +5,8 @@ import org.abondar.experimental.crackinginterview.Node;
 import org.junit.Assert;
 import org.junit.Test;
 
+import java.util.Arrays;
+
 public class ListsTest {
 
     private Lists ll = new Lists();
@@ -29,6 +31,7 @@ public class ListsTest {
         Node actualHead = ll.removeDups(head);
         Assert.assertEquals(expectedHead.getNext(),actualHead.getNext());
 
+
     }
 
     @Test
@@ -44,8 +47,73 @@ public class ListsTest {
         Node next3 = new Node(3);
         next2.setNext(next3);
 
-        Node expectedNext2 = next3;
         Node actualNext2 = ll.removeDups(head).getNext().getNext();
-        Assert.assertEquals(expectedNext2,actualNext2);
+        Assert.assertEquals(next3,actualNext2);
     }
+
+
+    @Test
+    public void kToLastTest(){
+        Node head = new Node(1);
+
+        Node next = new Node(2);
+        head.setNext(next);
+
+        Node next1 = new Node(3);
+        next.setNext(next1);
+
+        Node next2 = new Node(4);
+        next1.setNext(next2);
+
+        Node next3 = new Node(5);
+        next2.setNext(next3);
+
+        Node res =ll.kToLast(head,2);
+
+        Assert.assertEquals(next2,res);
+    }
+
+
+    @Test
+    public void kToLastTestRetLast(){
+        Node head = new Node(1);
+
+        Node next = new Node(2);
+        head.setNext(next);
+
+        Node next1 = new Node(3);
+        next.setNext(next1);
+
+        Node next2 = new Node(4);
+        next1.setNext(next2);
+
+        Node next3 = new Node(5);
+        next2.setNext(next3);
+
+        Node res =ll.kToLast(head,1);
+
+        Assert.assertEquals(next3,res);
+    }
+
+    @Test
+    public void kToLastTestRetNull(){
+        Node head = new Node(1);
+
+        Node next = new Node(2);
+        head.setNext(next);
+
+        Node next1 = new Node(3);
+        next.setNext(next1);
+
+        Node next2 = new Node(4);
+        next1.setNext(next2);
+
+        Node next3 = new Node(5);
+        next2.setNext(next3);
+
+        Node res =ll.kToLast(head,7);
+
+        Assert.assertEquals(null,res);
+    }
+
 }
