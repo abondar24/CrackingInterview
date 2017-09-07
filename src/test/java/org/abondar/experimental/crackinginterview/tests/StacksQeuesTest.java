891e6a1a3803c3d000cca16ba5b154338a869935
abondar24 <abondar1992@gmail.com>
2017-09-07 15:27:43 +0200

    Stacks and Queues base

---
+++ src/test/java/org/abondar/experimental/crackinginterview/tests/StacksQeuesTest.java
@@ -0,0 +1,9 @@
+package org.abondar.experimental.crackinginterview.tests;
+
+import org.abondar.experimental.crackinginterview.StacksQeues;
+
+public class StacksQeuesTest {
+
+    private StacksQeues sq = new StacksQeues();
+
+}
