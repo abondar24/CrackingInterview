050035a56d2510e3c088405762360ede73bb7611
Alex Bondar <abondar1992@gmail.com>
2024-06-09 00:25:43 +0200

    sorting algs refactoring

--- src/test/java/org/abondar/experimental/algorithms/SortingAlgTest.java
+++ src/test/java/org/abondar/experimental/algorithms/SortingAlgTest.java
@@ -1,38 +1,42 @@
 package org.abondar.experimental.algorithms;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class SortingAlgTest {
 
-    private final int[] data = new int[]{5,7,12,34,5,2,1};
+    private static final int[] data = new int[]{5,7,12,34,5,2,1};
+    private static final int[] expected = new int[]{1,2,5,5,7,12,34};
+
     private final SortingAlgs su = new SortingAlgs();
+
+
+    static Stream<Arguments> sortParams() {
+        return Stream.of(
+                Arguments.of(data,expected),
+                Arguments.of(new int[]{ 121, 432, 564, 23, 1, 45, 788 },new int[]{ 1, 23, 45, 121, 432, 564, 788 })
+        );
+    }
 
     @Test
     public void insertSortTest(){
         int []actualRes = su.insertionSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void heapSortTest(){
         int []actualRes = su.heapSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
 
@@ -40,130 +44,60 @@ public class SortingAlgTest {
     public void quickSortTest(){
         int []actualRes = su.quickSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void mergeSortTest(){
         int []actualRes = su.mergeSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void selectionSortTest(){
         int []actualRes = su.selectionSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void countingSortTest(){
         int []actualRes = su.countingSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
-    @Test
-    public void radixSortTest(){
+
+    @ParameterizedTest
+    @MethodSource("sortParams")
+    public void radixSortTest(int[] data, int[] expected){
         int []actualRes = su.radixSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
-    @Test
-    public void radixSortHundredTest(){
-        int[] data = { 121, 432, 564, 23, 1, 45, 788 };
-        int []actualRes = su.radixSort(data);
-
-        assertEquals(1L,actualRes[0]);
-        assertEquals(23L,actualRes[1]);
-        assertEquals(45L,actualRes[2]);
-        assertEquals(121L,actualRes[3]);
-        assertEquals(432L,actualRes[4]);
-        assertEquals(564L,actualRes[5]);
-        assertEquals(788L,actualRes[6]);
-    }
-
-
-    @Test
-    public void bucketSortTest(){
+    @ParameterizedTest
+    @MethodSource("sortParams")
+    public void bucketSortTest(int[] data, int[] expected){
         int []actualRes = su.bucketSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
-
-        int[] data = { 121, 432, 564, 23, 1, 45, 788 };
-        actualRes = su.bucketSort(data);
-
-        assertEquals(1L,actualRes[0]);
-        assertEquals(23L,actualRes[1]);
-        assertEquals(45L,actualRes[2]);
-        assertEquals(121L,actualRes[3]);
-        assertEquals(432L,actualRes[4]);
-        assertEquals(564L,actualRes[5]);
-        assertEquals(788L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void bucketFloatSortTest(){
         double[] data = { 0.42, 0.32, 0.33, 0.52, 0.37, 0.47, 0.51 };
+        double[] expected = {0.32,0.33,0.37,0.42,0.47,0.51,0.52};
         double[] actualRes = su.bucketSort(data);
 
-        assertEquals( 0.32,actualRes[0],0.01);
-        assertEquals(0.33,actualRes[1],0.01);
-        assertEquals(0.37,actualRes[2],0.01);
-        assertEquals(0.42,actualRes[3],0.01);
-        assertEquals(0.47,actualRes[4],0.01);
-        assertEquals(0.51,actualRes[5],0.01);
-        assertEquals(0.52,actualRes[6],0.01);
+        assertArrayEquals(expected,actualRes);
     }
 
     @Test
     public void bubbleSortTest(){
         int []actualRes = su.bubbleSort(data);
 
-        assertEquals(1L,actualRes[0]);
-        assertEquals(2L,actualRes[1]);
-        assertEquals(5L,actualRes[2]);
-        assertEquals(5L,actualRes[3]);
-        assertEquals(7L,actualRes[4]);
-        assertEquals(12L,actualRes[5]);
-        assertEquals(34L,actualRes[6]);
+        assertArrayEquals(expected,actualRes);
     }
 }
